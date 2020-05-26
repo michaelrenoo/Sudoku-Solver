11c0088c6b90bb1b8adfd41ffbf4a0b376ef04f8
Michael Reno <s0565907@htw-berlin.de>
2020-05-26 22:09:38 +0200

    Added a new function to check the possible numbers in the puzzle

--- Sudoku_Solver.py
+++ Sudoku_Solver.py
@@ -9,4 +9,25 @@ sudoku = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
           [0, 2, 6, 0, 0, 0, 0, 9, 0],
           [0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
+
+# Function to detect possible answer to the empty sudoku tile (y-axis, x-axis, possible number)
+def possible_answer(y, x, num):
+    global sudoku  # Use the variable sudoku from outside of the function's scope
+    for i in range(0, 9):  # because we have 9 possible elements
+        if sudoku[y][i] == num:  # if the possible number is already in the y-axis:
+            return False
+    for i in range(0, 9):  # so it iterates once more
+        if sudoku[i][x] == num:  # if the possible number is already in the x-axis:
+            return False
+    xbox = (x//3) * 3  # group the elements in 3 boxes. used floor division to get either the number 0, 3 or 6
+    ybox = (y//3) * 3
+    for i in range(0, 3):  # only 0 - 2 to get the value of element in between 0, 3, 6 and 9
+        for j in range(0, 3):
+            if sudoku[xbox + i][ybox + j] == num:  # to make a box in the x- and y-axis
+                return False
+    return True  # if num is not in any of the requirements above, then the number might be the correct answer
+
+
+
+
 print(np.matrix(sudoku))
